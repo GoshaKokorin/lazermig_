b1894cf5b6a7b03593f48d6afafeb43439b604c1
Gosha Kokorin <go_2002@mail.ru>
2023-09-27 13:10:36 +0500

    add: CSRF_TRUSTED_ORIGINS

--- lazermig/settings.py
+++ lazermig/settings.py
@@ -1,13 +1,11 @@
 import os
 from pathlib import Path
 
-from django.utils.functional import cached_property
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", '03&5zuvszt3fp!+u2ragfz52$)lond(r1f-1l&=x4dgdye25h6')
 
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', 0)
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(' ')
 
@@ -108,9 +106,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
@@ -146,3 +147,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5779",
 ]
+
+CSRF_TRUSTED_ORIGINS = ['http://api.lasermig.ru', 'https://lasermig.ru']
